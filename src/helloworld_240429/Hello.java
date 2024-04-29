93b75b9d8c91ce79fd74248ff94122750ef80f1b
JumpingDragon <bori555@ewhain.net>
2024-04-29 16:20:05 +0900

    line 추가 (추가 업로드 연습)

--- src/helloworld_240429/Hello.java
+++ src/helloworld_240429/Hello.java
@@ -6,6 +6,7 @@ public class Hello {
 		System.out.println("Hello"); 
 		//다음줄로 넘길 수 있는 콘솔에 결과 프린트 -> 주의: 자바는 ""만 문자열에 사용
 		System.out.print("World!"); //다음 줄로 안 넘기는 print
+		System.out.print("World!");
 	}
 
 }
